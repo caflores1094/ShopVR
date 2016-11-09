a89cb176bcf1d66d56f44395885f786979f1c577
Marissa <marissaokada@gmail.com>
2016-11-09 14:59:41 -0800

    (feat) Establish connection to mysql database

---
+++ server/db/index.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+
+// Establish connection to mysql database
+var connection = mysql.createConnection({
+  user: 'root',
+  password: '123',
+  database: 'shopvr'
+});
+
+connection.connect();
+
+module.exports = connection;
