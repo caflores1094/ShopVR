14471c629a42d8c6c59de6ba2c9fed2b5e904e42
Marissa <marissaokada@gmail.com>
2016-11-09 19:23:53 -0800

    (feat) Add models for users and items

--- server/models/index.js
+++ server/models/index.js
@@ -4,9 +4,27 @@ module.exports = {
   users: {
     get: function(callback) {
       var queryStr = 'select * from users';
-      db.query(queryStr, function(err, result) {
+      db.query(queryStr, function(err, results) {
         callback(results);
+      });
+    },
+    post: function(params, callback) {
+      var queryStr = 'insert into users(name, email, gender, price, profile_pic) \
+                      values(?)';
+      db.query(queryStr, params, function(err, results) {
+        callback(err, results);
       });
     }
   },
+  items: {
+    get: function(callback) {
+      var queryStr = 'select * from items';
+      db.query(queryStr, function(err, results) {
+        callback(err, results);
+      });
+    },
+    post: function(params, callback) {
+      var queryStr = 'insert into '
+    }
+  }
 };
