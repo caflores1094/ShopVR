76574c5438281094294daa8febbc0ba74b2b9b27
Marissa <marissaokada@gmail.com>
2016-11-12 14:30:32 -0800

    Refactor server

--- server/index.js
+++ server/index.js
@@ -1,26 +1,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var db = require('./db');
-var path = require('path');
-var controller = require('./controllers/auth.js');
+var router = require('express').Router();
+
+var authController = require('./controllers/auth.js');
 
 var server = express();
+
 
 server.set('port', 3000)
 server.listen(server.get('port'), function () {
   console.log('Server listening');
 });
 
-
-
 server.use(bodyParser.json()); // for parsing application/json
 server.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlenco
 server.use(express.static(__dirname + '/../client'));
+console.log(__dirname);
+server.use(router);
 
-server.get('/auth/login', controller.login);
-
-server.get('*', function (request, response){
-  response.sendFile(path.resolve(__dirname, '../client', 'index.html'));
-})
+router.post('/login/facebook', authController.login);
+router.get('/', function(req, res) {
+  res.sendFile('/client/index.html', {root: __dirname + '/..'});
+});
 
 module.exports = server;
