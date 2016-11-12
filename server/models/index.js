0f54115641c4fea6223c04550a3c41eb3ea121a8
Marissa <marissaokada@gmail.com>
2016-11-12 12:27:12 -0800

    Add additional model for getOne

--- server/models/index.js
+++ server/models/index.js
@@ -8,25 +8,21 @@ module.exports = {
         callback(results);
       });
     },
-    post: function(params, callback) {
-      var queryStr = 'insert into users(name, email, gender, price, profile_pic) \
-                      values(?)';
-      db.query(queryStr, params, function(err, results) {
+    getOne: function(email, callback) {
+      var queryStr = 'select * from users where users.email=?'; 
+      db.query(queryStr, [email], function(err, results) {
+        if (err) {
+          console.log(err);
+        }
         callback(err, results);
-      });
-    }
-  },
-  items: {
-    get: function(callback) {
-      var queryStr = 'select * from items';
-      db.query(queryStr, function(err, results) {
-        callback(results);
-      });
+      })
     },
-    post: function(params, callback) {
-      var queryStr = 'insert into items(brand, item_name, price, pic, category_id, user_id) \
-                      values(?)';
-      db.query(queryStr, params, function(err, results) {
+    post: function(user, callback) {
+      console.log('user', user);
+      var newQueryStr = 'INSERT INTO users (name, email, gender, locale, timezone, friends, fb_id, profile_pic) \
+                        VALUES ?';
+      db.query(newQueryStr, [user], function(err, results) {
+        console.log('insert', results);
         callback(err, results);
       });
     }
