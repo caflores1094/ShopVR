c10f877418cc1ab88c5452d9eaa7cd64a1c64c93
Marissa <marissaokada@gmail.com>
2016-11-12 14:59:22 -0800

    (feat) Add image upload

--- client/bundle.js
+++ client/bundle.js
@@ -27079,6 +27079,10 @@
 
 	var _Social2 = _interopRequireDefault(_Social);
 
+	var _ImageUpload = __webpack_require__(260);
+
+	var _ImageUpload2 = _interopRequireDefault(_ImageUpload);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -27102,6 +27106,7 @@
 	      return _react2.default.createElement(
 	        'div',
 	        null,
+	        _react2.default.createElement(_ImageUpload2.default, null),
 	        _react2.default.createElement(_QueryBox2.default, { user: this.props.user, store: this.props.store }),
 	        _react2.default.createElement(_Feed2.default, { store: this.props.store }),
 	        _react2.default.createElement(_Social2.default, { store: this.props.store })
@@ -28590,6 +28595,119 @@
 	    }, last.apply(undefined, arguments));
 	  };
 	}
+
+/***/ },
+/* 260 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _react = __webpack_require__(1);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var ImageUpload = function (_React$Component) {
+	  _inherits(ImageUpload, _React$Component);
+
+	  function ImageUpload(props) {
+	    _classCallCheck(this, ImageUpload);
+
+	    var _this = _possibleConstructorReturn(this, (ImageUpload.__proto__ || Object.getPrototypeOf(ImageUpload)).call(this, props));
+
+	    _this.state = { file: '', imagePreviewUrl: '' };
+	    return _this;
+	  }
+
+	  _createClass(ImageUpload, [{
+	    key: '_handleSubmit',
+	    value: function _handleSubmit(e) {
+	      e.preventDefault();
+	      // TODO: do something with -> this.state.file
+	      console.log('handle uploading-', this.state.file);
+	    }
+	  }, {
+	    key: '_handleImageChange',
+	    value: function _handleImageChange(e) {
+	      var _this2 = this;
+
+	      e.preventDefault();
+
+	      var reader = new FileReader();
+	      var file = e.target.files[0];
+
+	      reader.onloadend = function () {
+	        _this2.setState({
+	          file: file,
+	          imagePreviewUrl: reader.result
+	        });
+	      };
+
+	      reader.readAsDataURL(file);
+	    }
+	  }, {
+	    key: 'render',
+	    value: function render() {
+	      var _this3 = this;
+
+	      var imagePreviewUrl = this.state.imagePreviewUrl;
+
+	      var $imagePreview = null;
+	      if (imagePreviewUrl) {
+	        $imagePreview = _react2.default.createElement('img', { src: imagePreviewUrl });
+	      } else {
+	        $imagePreview = _react2.default.createElement(
+	          'div',
+	          { className: 'previewText' },
+	          'Please select an Image for Preview'
+	        );
+	      }
+
+	      return _react2.default.createElement(
+	        'div',
+	        { className: 'previewComponent' },
+	        _react2.default.createElement(
+	          'form',
+	          { onSubmit: function onSubmit(e) {
+	              return _this3._handleSubmit(e);
+	            } },
+	          _react2.default.createElement('input', { className: 'fileInput', type: 'file', onChange: function onChange(e) {
+	              return _this3._handleImageChange(e);
+	            } }),
+	          _react2.default.createElement(
+	            'button',
+	            { className: 'submitButton', type: 'submit', onClick: function onClick(e) {
+	                return _this3._handleSubmit(e);
+	              } },
+	            'Upload Image'
+	          )
+	        ),
+	        _react2.default.createElement(
+	          'div',
+	          { className: 'imgPreview' },
+	          $imagePreview
+	        )
+	      );
+	    }
+	  }]);
+
+	  return ImageUpload;
+	}(_react2.default.Component);
+
+	exports.default = ImageUpload;
 
 /***/ }
 /******/ ]);
