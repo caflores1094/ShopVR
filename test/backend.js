50023ae3470e7a97a61e93d3cf274c3b3c028072
Marissa <marissaokada@gmail.com>
2016-11-14 12:15:59 -0800

    (dev) Add in first test

--- test/backend.js
+++ test/backend.js
@@ -4,4 +4,27 @@ var expect = require('chai').expect;
 
 describe('Persistant Server', function(){
   var dbConnection;
+
+  before(function(done) {
+    user: 'root',
+    password: '123',
+    database: 'shopvr'
+  });
+
+  dbConnection.connect();
+
+  var tableName = 'users';
+
+  dbConnection.query('truncate ' + tablename, done);
+
+  after(function() {
+    dbConnection.end();
+  });
+
+  it('Should insert a new user into the database', function(done) {
+    request({
+      method: 'POST',
+      uri: 'http://localhost:3000/login/facebook'
+    })
+  });
 });
