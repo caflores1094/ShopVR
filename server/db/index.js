a69329cecb47984d1bed7ea0964c00a79616d0f7
Carlos Flores <caflores1094@gmail.comgit config --global core.autocrlf inputgit config --global core.safecrlf truepwdgit config --global user.email caflores1094@gmail.com>
2016-11-15 09:46:12 -0800

    Test merge again

--- server/db/index.js
+++ server/db/index.js
@@ -2,6 +2,12 @@ var mysql = require('mysql');
 
 // Establish connection to mysql database
 var connection = mysql.createConnection({
+  user: 'root',
+  password: '1107',
+  database: 'shopvr'
+});
+
+var connection = mysql.createConnection({
   user: 'newuser',
   password: 'password',
   database: 'shopvr'
