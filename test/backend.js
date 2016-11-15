dce4785c909100d81180a4e8461753c0f6b59edb
Marissa <marissaokada@gmail.com>
2016-11-15 10:38:27 -0800

    (dev) Create backlog of minor issues

--- test/backend.js
+++ test/backend.js
@@ -84,7 +84,6 @@ describe('Database', function() {
       });
       it('Should have a record of the insert', function(done) {
         dbConnection.query('SELECT * FROM users', function(err, results) {
-          console.log('result', results);
           expect(results.length).to.equal(1);
           expect(results[0].name).to.equal('Bob Bob');
           done();
