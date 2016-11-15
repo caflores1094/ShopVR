5716d8e94f7581c0b1fa3ee8da8976d593d69a7e
Anne Lee <annelee612@gmail.com>
2016-11-15 11:55:08 -0800

    Add secure server

--- server/db/index.js
+++ server/db/index.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 
 // Establish connection to mysql database
 var connection = mysql.createConnection({
-  user: 'root',
-  password: '1107',
+  user: 'newuser',
+  password: 'password',
   database: 'shopvr'
 });
 
--- server/index.js
+++ server/index.js
@@ -2,17 +2,26 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var router = require('express').Router();
 var db = require('./db');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
 
 var authController = require('./controllers/auth.js');
 var picController = require('./controllers/pic.js');
 
+//this is for HTTPS
+var options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
 var server = express();
 
-
-server.set('port', 3000)
-server.listen(server.get('port'), function () {
-  console.log('Server listening');
-});
+var normalServer = http.createServer(server).listen(3000);
+var secureServer = https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  res.end("secure https server running on 8000");
+}).listen(8000);
 
 server.use(bodyParser.json()); // for parsing application/json
 server.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlenco
