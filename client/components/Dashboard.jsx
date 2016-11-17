3d962774e76ab12005be26172c5a11a66d8f8301
Marissa <marissaokada@gmail.com>
2016-11-17 10:52:47 -0800

    Add styling

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -9,8 +9,19 @@ class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      imageUpload: "displayNone",
-      search: "displayNone"
+      imageUpload: "none",
+      search: "none"
+    }
+  }
+
+  showTool(name, e) {
+    var toShow = {};
+    if (this.state[name] === "none") {
+      toShow[name] = "block";
+      this.setState(toShow);
+    } else {
+      toShow[name] = "none";
+      this.setState(toShow);
     }
   }
 
@@ -20,13 +31,19 @@ class Dashboard extends React.Component {
         <div className="dashboard-hero"></div>
         <div className="dashboard-border"></div>
         <button className="dashboard-button" onClick={() => browserHistory.push('/vr')}>View In VR</button>
-        <div className="dashboard-upload">
-          <button className="upload-button">Upload Image</button>
-          <ImageUpload style={{ display: this.state.imageUpload }} user={this.props.user} setFeed={this.props.setFeed}/>
-        </div>
-        <div className="dashboard-search">
-          <button className="search-button">Search</button>
-          <QueryBox style={{ display: this.state.search }} user={this.props.user} setFeed={this.props.setFeed}/>
+        <div className="dashboard-tools">
+          <div className="dashboard-upload">
+            <button onClick={this.showTool.bind(this, 'imageUpload')} className="upload-button">Upload Image</button>
+            <div name="imageUpload" className="image-upload" style={{display: this.state.imageUpload}}>
+              <ImageUpload user={this.props.user} setFeed={this.props.setFeed}/>
+            </div>
+          </div>
+          <div className="dashboard-search">
+            <button onClick={this.showTool.bind(this, 'search')} className="search-button">Search</button>
+            <div name="search" className="query-box" style={{display: this.state.search}}>
+              <QueryBox style={{ display: this.state.search }} user={this.props.user} setFeed={this.props.setFeed}/>
+            </div>
+          </div>
         </div>
         
         <div className="dashboard-feed">
