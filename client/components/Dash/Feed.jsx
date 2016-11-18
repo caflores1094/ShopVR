f39e387c7567f808ec79d7674b6f601f529b661e
Anne Lee <annelee612@gmail.com>
2016-11-18 15:27:50 -0800

    fixed previous button

--- client/components/Dash/Feed.jsx
+++ client/components/Dash/Feed.jsx
@@ -40,9 +40,8 @@ class Feed extends React.Component {
       });
   }
   previous() {
-    if (count > 0) {
+    if (count >= 50) {
       count -= 50;
-
     }
     this.setState({
       offset: count
@@ -50,7 +49,7 @@ class Feed extends React.Component {
     var context = this;
     var gender = this.props.user.gender === 'male' ? "men" : "women"
 
-    axios.get("http://api.shopstyle.com/api/v2/products/?pid=uid4025-36835155-23&offset=" + this.state.offset + "&fts=" + gender + "&limit=50")
+    axios.get("http://api.shopstyle.com/api/v2/products/?pid=uid4025-36835155-23&offset=" + count + "&fts=" + gender + "&limit=50")
       .then(function (response) {
         context.props.setFeed(response.data.products);
       })
