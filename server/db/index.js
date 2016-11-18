f304f08945f9423cb94201fa21a49798b0cd99a2
Carlos Flores <caflores1094@gmail.com>
2016-11-18 11:11:17 -0800

    Configure database to connect to heroku clearDB

--- server/db/index.js
+++ server/db/index.js
@@ -2,12 +2,13 @@ var mysql = require('mysql');
 
 // Establish connection to mysql database
 var connection = mysql.createConnection({
-  user: 'newuser',
-  password: 'password',
-  database: 'shopvr'
+  user: 'ba6adb9eb41809',
+  password: '7decb2ed',
+  host: 'us-cdbr-iron-east-04.cleardb.net',
+  database: 'heroku_8f76abdea29d9e5'
 });
 
-// connection.connect();
+connection.connect();
 // connection.end();
 
 module.exports = connection;
