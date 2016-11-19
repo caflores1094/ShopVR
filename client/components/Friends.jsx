8a804abcf569aeee6b6ca120f6d7739abed8bfb1
Marissa <marissaokada@gmail.com>
2016-11-18 20:13:02 -0800

    Create FriendsWishlist components

--- client/components/Friends.jsx
+++ client/components/Friends.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import axios from 'axios';
+import FriendsWishlist from './FriendsWishlist.jsx';
 
 class Friends extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      names: [],
+      friends: {}
+    }
   }
 
   getFriends() {
     var friends = {};
+    var names = [];
+    var context = this;
     axios.post('/api/getFriends', {id: this.props.user.id})
       .then(function(results) {
         results.data.forEach(function(item) {
@@ -15,10 +22,15 @@ class Friends extends React.Component {
             .then(function(user) {
               if (friends[user.data[0].name] === undefined) {
                 friends[user.data[0].name] = { wishlist: [item] };
+                names.push(user.data[0].name);
               } else {
                 friends[user.data[0].name]['wishlist'].push(item);
               }
-              console.log('friends obj', friends);
+
+              context.setState({
+                names: names,
+                friends: friends
+              });
             });
         });
       });
@@ -30,7 +42,9 @@ class Friends extends React.Component {
 
   render() {
     return(
-      <div>Hello, Friends</div>
+      <div className="friends-wishlist">
+        <FriendsWishlist wishlist={this.state.friends} names={this.state.names} />
+      </div>
     );
   }
 }
