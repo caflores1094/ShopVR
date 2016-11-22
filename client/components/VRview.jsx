ba6f86e311d9406267e58d322f97e6b704bea840
Carlos Flores <caflores1094@gmail.com>
2016-11-22 11:04:10 -0800

    basic organizing of clothing in VR by categories

--- client/components/VRview.jsx
+++ client/components/VRview.jsx
@@ -10,10 +10,25 @@ class VRview extends React.Component {
 
   render() {
     var context = this;
-    var vrFeed = this.props.feed.map((clothingObj)=> { 
+    var categories = {};
+    this.props.feed.forEach((elem)=>{
+      if(elem.categories[0].shortName in categories){
+        categories[elem.categories[0].shortName].push(elem);
+      } else{
+        categories[elem.categories[0].shortName] = [elem]
+      }
+    });
+    var firstFeed = [];
+    for(var category in categories){
+      categories[category].forEach((elem)=>{
+        firstFeed.push(elem);
+      })
+    }
+    console.log(firstFeed)
+
+    var vrFeed = firstFeed.map((clothingObj)=> { 
         return( [clothingObj.image.sizes.IPhoneSmall.url, clothingObj.image.sizes.IPhoneSmall.actualHeight / clothingObj.image.sizes.IPhoneSmall.actualWidth, clothingObj.priceLabel])
       });
-
     return (
       <div>
         <Sharebar user={this.props.user}/>
