38010f265f7d6ab2fdab51dba5c35996a7ef5a14
Victor Mu <vic.mgs@gmail.com>
2016-11-22 11:25:13 -0800

    Add sockets and chat functionality

--- client/components/Friends.jsx
+++ client/components/Friends.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import FriendsWishlist from './FriendsWishlist.jsx';
+import ChatText from './ChatText.jsx';
 
 var friends = {};
 var names = [];
@@ -9,8 +10,11 @@ class Friends extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      wishlist: []
-    }
+      wishlist: [],
+      chatText: '',
+      chatLog: [],
+      room: ''
+    };
   }
 
   getFriends(callback) {
@@ -27,7 +31,34 @@ class Friends extends React.Component {
   }
 
   componentDidMount() {
+    var context = this;
+    this.socket = io();
+    this.socket.on('chat message', function(msg){
+      if (msg.room === context.state.room) {
+        var chat = context.state.chatLog;
+        if (chat.length > 49) {
+          chat = chat.slice(1);
+        }
+        chat.push(msg);
+        context.setState({chatLog: chat});
+      }
+    });
+
     this.getFriends();
+  }
+
+  roomSubmit(e) {
+    e.preventDefault();
+    this.setState({room: this.refs.room.value});
+  }
+
+  chatSubmit(e) {
+    e.preventDefault();
+    if (this.state.chatText !== '' && this.state.room !== '') {
+      this.socket.emit('chat message', {room: this.state.room, user: this.props.user.name, text: this.state.chatText});
+      this.refs.text.value = '';
+      this.setState({chatText: ''});
+    }
   }
 
   render() {
@@ -35,6 +66,19 @@ class Friends extends React.Component {
       <div className="friends-container">
         <div className="friends-header">See Friends' Wishlists</div>
         <FriendsWishlist list={this.state.wishlist} />
+        <div className="chatbox">
+          {
+            this.state.chatLog.map((obj, i) => (
+              <ChatText key={i} text={obj.text} name={obj.user} user={this.props.user}/>
+              ))
+          }
+        </div>
+        <form onSubmit={(e) => this.roomSubmit(e)}>
+          <input name='room' ref='room'/><button>Set Room</button>
+        </form>
+        <form onSubmit={(e) => this.chatSubmit(e)}>
+          <input name='text' ref='text' onChange={(e) => this.setState({chatText: e.target.value})}/><button>Send</button>
+        </form>
       </div>
     );
   }
