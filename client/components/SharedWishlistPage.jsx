6243ebd5b990317f4c56040e0a13a392dd616e74
Marissa <marissaokada@gmail.com>
2016-11-30 19:19:35 -0800

    Add styling to chat

--- client/components/SharedWishlistPage.jsx
+++ client/components/SharedWishlistPage.jsx
@@ -13,16 +13,18 @@ class SharedWishlistPage extends React.Component {
       <div>
         {this.props.friendFound ? 
         <div>
-          <div className="shared-wishlist-header">{this.props.friend + "'s"} Wishlist</div>
-          <div className="share-wishlist"></div>
-          <img className="shared-wishlist-photo" src={this.props.photo}></img>
-
-          {
-            this.props.list.map((picObj) => (
-                <SharedWishlistItem key={picObj.pic_name} picObj={picObj} getWishList={context.props.getWishList}/>
+          <div className="shared-wishlist-headline">
+            <div className="shared-wishlist-header">{this.props.friend + "'s"}<br/> Wishlist</div>
+            <div className="shared-wishlist-pic"><img className="shared-wishlist-photo" src={this.props.photo}></img></div>
+          </div>
+          <div className="shared-wishlist-container">
+            {
+              this.props.list.map((picObj) => (
+                  <SharedWishlistItem key={picObj.pic_name} picObj={picObj} getWishList={context.props.getWishList}/>
+                )
               )
-            )
-          }
+            }
+          </div>
         </div> : <div className="fourofour">404 not found - you may not have permission for the page you're looking for</div>}
       </div>
     );
